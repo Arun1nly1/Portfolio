0cb6bc82e391272169d41838f1389ab993f266c1
Arun Chapagain <arunsansar123@gmail.com>
2019-12-10 23:04:39 +0545

    First push

---
+++ port_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/arun/DATA/Udemy courses/Django project udemy/Portfolio/port_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
